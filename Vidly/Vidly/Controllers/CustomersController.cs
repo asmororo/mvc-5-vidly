d242ee2e78dbb539f3bdfe954c7de7dd57d74ce4
Anderson Mororo <asmororo@gmail.com>
2018-08-22 22:32:55 +0100

    First Commit

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult AllCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Willians" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        //[Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Willians" }
+            };
+
+            var customer = customers.Where(c => c.Id == id);
+
+            string name = "Teste";
+
+            return Content(name);
+        }
+    }
+}
